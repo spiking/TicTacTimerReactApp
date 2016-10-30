8d3f216eb49232f39224ccbbbb32e88c2fe4e245
Adam Thuvesen <a.thuvesen@gmail.com>
2016-10-30 14:08:32 +0100

    Nav fixes

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,10 +1,31 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Navigation = {} => {
+var Navigation = () => {
   return (
-
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/spiking" target="_blank">Adam</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
-modules.export = Naviation;
+module.exports = Navigation;
